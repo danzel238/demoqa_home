4f7ceac0aec0504bbef3d9d1098a97e9cf687198
danzel238 <djban778@gmail.com>
2025-08-27 18:07:19 +0300

    Create elements.py

---
+++ pages/elements.py
@@ -0,0 +1,19 @@
+from components.components import WebElement
+from pages.base_page import BasePage
+
+
+class ElementsPage(BasePage):
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/elements'
+        super().__init__(driver, self.base_url)
+
+        self.btns_first_menu = WebElement(driver, 'div:nth-child(1) > div > ul > li')
+        self.icon = WebElement(driver, 'header > a > img')
+        self.btn_sidebar_first = WebElement(driver, 'div:nth-child(1) > span > div')
+        self.btn_sidebar_first_textbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-0 > span')
+        self.btn_sidebar_first_checkbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-1 > span')
+    def equal_url(self):
+        if self.get_url() == self.base_url:
+            return True
+        else:
+            return False
