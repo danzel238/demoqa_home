7ca9006f723556d529babf4899f6515d6619688c
danzel238 <djban778@gmail.com>
2025-08-27 18:08:47 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,8 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture(scope="function")
-def driver():
+def browser():
     driver = webdriver.Chrome()
     yield driver
+    browser.set_window_size(1000,1000)
     driver.quit()
