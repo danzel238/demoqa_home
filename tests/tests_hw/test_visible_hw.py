ff8e52139f54c4ef9e68a4940d5f089881a092c4
danzel238 <djban778@gmail.com>
2025-08-27 16:03:45 +0300

    test_visible_hw.py

--- tests/tests_hw/test_visible_hw.py
+++ tests/tests_hw/test_visible_hw.py
@@ -10,13 +10,13 @@ def test_visible_accordion(browser):
     page = Accordion(browser)
     page.visit()
 
-
     WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located(page.SECTION1_CONTENT_P))
-  
+        EC.visibility_of_element_located(page.SECTION1_CONTENT_P)
+    )
     assert page.section1_content_p.is_displayed(), "Ожидали, что section1Content > p виден по умолчанию"
 
     page.section1_heading.click()
+    time.sleep(2)
 
     assert not page.section1_content_p.is_displayed(), "Ожидали, что section1Content > p скрыт после клика"
 
@@ -24,13 +24,16 @@ def test_visible_accordion(browser):
 def test_visible_accordion_default(browser):
     page = Accordion(browser)
     page.visit()
-  
+
     WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located(page.SECTION2_P1))
+        EC.presence_of_element_located(page.SECTION2_P1)
+    )
     WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located(page.SECTION2_P2))
+        EC.presence_of_element_located(page.SECTION2_P2)
+    )
     WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located(page.SECTION3_P))
+        EC.presence_of_element_located(page.SECTION3_P)
+    )
 
     assert not page.section2_p1.is_displayed(), "Ожидали, что section2Content > p:nth-child(1) скрыт по умолчанию"
     assert not page.section2_p2.is_displayed(), "Ожидали, что section2Content > p:nth-child(2) скрыт по умолчанию"
