e5940190fa623d5d66514482684624d4d9f16719
danzel238 <djban778@gmail.com>
2025-08-27 18:08:12 +0300

    Create components.py

---
+++ components.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+class WebElement:
+    def __init__(self, driver, locator=''):
+        self.driver = driver
+        self.locator = locator
+
+    def click(self):
+        self.find_element().click()
+
+    def find_element(self,locator):
+        return self.driver.find_element(By.CSS_SELECTOR,self.locator)
+
+
+    def find_elements(self,locator):
+        return self.driver.find_elements(By.CSS_SELECTOR,self.locator)
+
+    def exist(self):
+        try:
+            self.find_element()
+        except NoSuchElementException:
+            return False
+        else:
+            return True
+
+    def get_text(self):
+            return str(self.find_element().text)
+
+
+    def visible(self):
+        return self.find_element().is_displayed()
+
+    def check_count_elements(self,count:int) -> bool:
+        if len(self.find_elements()) == count:
+            return True
+        else:
+            return False
+
+    def send_keyes(self, text:str):
+        self.find_element().send_keyes(text)
